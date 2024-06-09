5b4778a4db748daed2ab95a8fbf08959a66320b5
alchemist1111 <emoyoeugine@gmail.com>
2024-06-09 10:03:25 +0300

    added setters and getters for article,author, magazine

--- models/author.py
+++ models/author.py
@@ -4,4 +4,16 @@ class Author:
         self.name = name
 
     def __repr__(self):
-        return f'<Author {self.name}>'
+        return f'<Author {self.id}: {self.name}>'
+    
+    @property
+    def name(self):
+        # Getter for the name property
+        return self.name
+    
+    @name.setter
+    def name(self, value):
+         # Setter for the name property
+        # Check if value is a string and the name has not been set before
+        if isinstance(value, str) and not hasattr(self, 'name'):
+         self.name = value
--- models/magazine.py
+++ models/magazine.py
@@ -5,4 +5,30 @@ class Magazine:
         self.category = category
 
     def __repr__(self):
-        return f'<Magazine {self.name}>'
+        return f'<Magazine {self.id}: {self.name}, {self.category}>'
+    
+    @property
+    def name(self):
+        # Getter for the name property
+        return self.name
+    
+    @name.setter
+    def name(self, value):
+        # Setter for the name property
+        # Check if the value is a string with length between 2 and 16 characters
+        if not isinstance(value, str) or len(value) < 2 or len(value) > 16:
+            raise ValueError("Magazine name must be a string between 2 and 16 characters")
+        self._name = value 
+
+    @property
+    def category(self):
+        # Getter for the category property
+        return self._category
+
+    @category.setter
+    def category(self, value):
+        # Setter for the category property
+        # Check if the value is a non-empty string
+        if not isinstance(value, str) or len(value) == 0:
+            raise ValueError("Category must be a non-empty string.")
+        self._category = value    
