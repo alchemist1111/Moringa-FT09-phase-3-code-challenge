ad0f53dcbe0b4bdf2d8936791327757cca8f1364
alchemist1111 <emoyoeugine@gmail.com>
2024-06-19 17:13:24 +0300

    updated author file

--- models/author.py
+++ models/author.py
@@ -1,19 +1,11 @@
-from database.connection import get_db_connection
-
 class Author:
     def __init__(self, id, name):
         self._id = id
-        self.name = name
+        self._name = name
 
     @property
     def id(self):
         return self._id
-    
-    @id.setter
-    def id(self, value):
-        if not isinstance(value, int):
-            raise ValueError("Id must be an integer.")
-        self._id = value
 
     @property
     def name(self):
@@ -21,31 +13,35 @@ class Author:
 
     @name.setter
     def name(self, value):
-        if not isinstance(value, str) or len(value) == 0:
-            raise ValueError("Name must be a non-empty string.")
+        if not isinstance(value, str):
+            raise TypeError("Name must be a string.")
+        if len(value) == 0:
+            raise ValueError("Name must not be empty.")
+        if hasattr(self, '_name'):
+            raise AttributeError("Name cannot be changed after instantiation.")
         self._name = value
 
-    def articles(self):
-        from models.article import Article
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM articles WHERE author_id = ?', (self.id,))
-        rows = cursor.fetchall()
-        conn.close()
-        return [Article(row['id'], row['title']) for row in rows]
+    def create_author(self, cursor):
+        cursor.execute("INSERT INTO authors (name) VALUES (?)", (self._name,))
+        self._id = cursor.lastrowid
 
-    def magazines(self):
-        from models.magazine import Magazine 
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute('''
-            SELECT DISTINCT magazines.* FROM magazines
-            JOIN articles ON articles.magazine_id = magazines.id
+    @classmethod
+    def get_all_authors(cls, cursor):
+        cursor.execute("SELECT * FROM authors")
+        authors_data = cursor.fetchall()
+        return [cls(id=row[0], name=row[1]) for row in authors_data]
+
+    def articles(self, cursor):
+        cursor.execute("SELECT * FROM articles WHERE author_id = ?", (self._id,))
+        articles_data = cursor.fetchall()
+        return articles_data
+
+    def magazines(self, cursor):
+        cursor.execute("""
+            SELECT magazines.*
+            FROM magazines
+            JOIN articles ON magazines.id = articles.magazine_id
             WHERE articles.author_id = ?
-        ''', (self.id,))
-        rows = cursor.fetchall()
-        conn.close()
-        return [Magazine(row['id'], row['name'], row['category']) for row in rows]
-
-    def __repr__(self):
-        return f'<Author {self.name}>'
+        """, (self._id,))
+        magazines_data = cursor.fetchall()
+        return magazines_data
